67d674e6ac4a3d3d27a4aa277959706cb49bef81
YuriJ <ytauchi@student.holmesglen.edu.au>
2021-05-26 20:42:08 +1000

    Login Api Implemented;

---
+++ src/controllers/login.js
@@ -0,0 +1,36 @@
+const _ = require("lodash");
+const createDebug = require("debug");
+const { createValidator, jwtSignUser } = require("./utils");
+const { default: User, validateLogin } = require("../models/User");
+
+const debug = createDebug("app:controller:login");
+
+module.exports = {
+  validate: createValidator(validateLogin),
+
+  post: async (req, res) => {
+    try {
+      const {
+        body: { email, password },
+      } = req;
+      const user = await User.findOne({ email }).exec();
+      if (_.isNil(user)) {
+        debug("User with given email does not exist:", email);
+        res.status(401).send({ error: "User Not Available" });
+        return;
+      }
+      const isPassValid = await user.validatePassword(password);
+      if (isPassValid) {
+        const JWT = jwtSignUser(user.toJSON());
+        res.set({ JWT }).send({ user });
+        return;
+      }
+      debug("Unsuccessful login for user email:", email);
+      res.status(401).send({ error: "Password Invalid" });
+      return;
+    } catch (err) {
+      debug("Error at login POST call:", err);
+      res.status(500).send({ error: err });
+    }
+  },
+};
